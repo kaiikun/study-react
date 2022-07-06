d8f316a87de5f5ee992e446656bd3598abad1220
kaiikun <108375764+kaiikun@users.noreply.github.com>
2022-07-06 09:30:31 +0900

    useEffectとuseStateを使って背景色を変え、カウントアップのボタンを作りましたよ

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,26 +3,32 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
-  const foo = "1";
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  })
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount((count) => count + 1)
+  }
+
+  useEffect(()=>{
+    document.body.style.background = "lightblue";
+    return() => {
+      document.body.style.background = "";
+    };
+  });
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
       <Header />
-      <a 
-        href="/about"
+      <div>{count}</div>
+      <button 
         onClick={handleClick}>ボタン
-      </a>
-      
+      </button>
       <Main page="index" />
      <Footer />
     </div>
