72384b3390f32627aa1efbf5bc0203bb8afcd3b2
kaiikun <108375764+kaiikun@users.noreply.github.com>
2022-08-01 09:33:50 +0900

    APIを表示する形に変えました

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,53 +3,69 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react' 
 
 
-const useCounter = () => {
-  const [count, setCount] = useState(1);
-  const [isShow,setIsShow] = useState(true);
+/* カウントのReact講座で使った */
+// const useCounter = () => {
+//   const [count, setCount] = useState(1);
+//   const [isShow,setIsShow] = useState(true);
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1)
-  }
+//   const handleClick = (e) => {
+//     setCount((count) => count + 1)
+//   }
   
-  const handleDisplay = () => {
-    setIsShow( (isShow) => {
-      return !isShow;
-    })
-  }
+//   const handleDisplay = () => {
+//     setIsShow( (isShow) => {
+//       return !isShow;
+//     })
+//   }
 
-  return(count, isShow , handleClick, handleDisplay)
-}
+//   return(count, isShow , handleClick, handleDisplay)
+// }
 
 export default function Home(props) { 
+  const [posts,setPosts] = useState([]);
 
-  console.log(props)
-  const {count, isShow , handleClick, handleDisplay} = useCounter()
-  const [text, setText] = useState("");
-  const [array,setArray] = useState([]);
+  const getPosts = useCallback(async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    console.log(json);
+    setPosts(json);
+  },[])
 
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ奴はだめ")
-      }
-      const newArray = [...prevArray,text];
-      return newArray;
-    });
-  },[text]);
+// getPostsの実行をする関数の定義↓
+  useEffect(() => {
+    getPosts();
+  },[getPosts])
 
-  useEffect(()=>{
-    document.body.style.background = "lightblue";
-    return() => {
-      document.body.style.background = "";
-    };
-  });
 
-  const handleChange = (e) => {
-    setText(e.target.value)
-  }
+  /* カウントのReact講座で使った */
+  // console.log(props)
+  // const {count, isShow , handleClick, handleDisplay} = useCounter()
+  // const [text, setText] = useState("");
+  // const [array,setArray] = useState([]);
+
+  // const handleAdd = useCallback(() => {
+  //   setArray((prevArray) => {
+  //     if (prevArray.some((item) => item === text)) {
+  //       alert("同じ奴はだめ")
+  //     }
+  //     const newArray = [...prevArray,text];
+  //     return newArray;
+  //   });
+  // },[text]);
+
+  // useEffect(()=>{
+  //   document.body.style.background = "lightblue";
+  //   return() => {
+  //     document.body.style.background = "";
+  //   };
+  // });
+
+  // const handleChange = (e) => {
+  //   setText(e.target.value)
+  // }
 
   return (
     <div className={styles.container}>
@@ -57,25 +73,13 @@ export default function Home(props) {
         <title>Create Next App</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
-      <input 
-        type="text" 
-        value={text} 
-        onChange={handleChange}/>
-      <button 
-        onClick={handleClick}>ボタン
-      </button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-
-      <button onClick={handleAdd}>テェい</button>
-      <ul>{array.map(item => {
-        return(
-          <list key={item}>{item}</list>
-        )
-        
-      })}</ul>
-      <Main page="index" />
-     <Footer />
+      <ol>
+        {posts.map(post => {
+          return(
+            <li key={post.id}>{post.title}</li>
+          )
+        })}
+      </ol>
     </div>
   )
 }
